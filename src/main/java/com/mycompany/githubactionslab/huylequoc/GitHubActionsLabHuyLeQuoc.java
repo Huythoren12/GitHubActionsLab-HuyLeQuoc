ea867195ff85038fb01636bc620737897441ed16
huythoren12 <huyle@sheridancollege.ca>
2026-06-09 15:51:50 -0400

    Initial commit: Add workflow and build files

---
+++ src/main/java/com/mycompany/githubactionslab/huylequoc/GitHubActionsLabHuyLeQuoc.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.githubactionslab.huylequoc;
+
+/**
+ *
+ * @author HP
+ */
+public class GitHubActionsLabHuyLeQuoc {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
